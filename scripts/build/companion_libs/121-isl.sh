7924b58d168d4e8bfe0125d3a8be7d75de3dd8ed
Laurent Moss <laurent@laurentmoss.com>
2014-08-21 16:40:50 +0000

    Update download location for ISL
    The former location ftp://ftp.linux.student.kuleuven.be seems to be down, causing a series of timeouts.

--- scripts/build/companion_libs/121-isl.sh
+++ scripts/build/companion_libs/121-isl.sh
@@ -13,7 +13,7 @@ if [ "${CT_ISL}" = "y" ]; then
 # Download ISL
 do_isl_get() {
     CT_GetFile "isl-${CT_ISL_VERSION}"  \
-        ftp://ftp.linux.student.kuleuven.be/pub/people/skimo/isl/ \
+        http://isl.gforge.inria.fr \
         http://mirrors.kernel.org/sources.redhat.com/gcc/infrastructure
 }
 
